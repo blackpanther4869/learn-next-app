ca93ebe54449202e510382a2529ad70ca41fe312
blackpanther4869 <mba@willow.local>
2025-07-26 01:16:44 +0900

    feat: add new message and about link

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ export default function Home() {
       <h1>ようこそ！ Next.js Appへ</h1>
       <p>これは私の初めてのWebアプリケーションです。</p>
       <p>Vercelにデプロイしてみよう！</p>
+      <p>🎉 コードを変更して自動デプロイを体験しよう！ 🎉</p> {/* この行を追加 */}
+      <p><a href="/about">このアプリについて</a></p> {/* aboutページへのリンクを追加 */}
     </main>
   );
 }
