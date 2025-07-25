ec8f9e4902d89877a81ad2659ba9d577d7fc3b9c
mask <mba@willow.local>
2025-07-26 00:19:38 +0900

    feat: initial commit

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main>
+      <h1>このアプリについて</h1>
+      <p>これはNext.jsとVercelのハンズオンのために作成されました。</p>
+      <p><a href="/">トップページに戻る</a></p>
+    </main>
+  );
+}
