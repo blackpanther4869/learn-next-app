4824ad903240cc778326ea21cd76c0361a5ae9fa
blackpanther4869 <mba@willow.local>
2025-07-26 01:05:33 +0900

    feat: ルートへの<a>タグによる直接リンクづけを避け、<Link>を使う

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <main>
       <h1>このアプリについて</h1>
       <p>これはNext.jsとVercelのハンズオンのために作成されました。</p>
-      <p><a href="/">トップページに戻る</a></p>
+      <p><Link href="/"><a>トップページに戻る</a></Link></p>
     </main>
   );
 }
